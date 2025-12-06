5180df73073ad3b5d012bbc550093b24e589c726
PRISCILLAH GITERO <priscillargitero60@gmail.com>
2025-12-06 18:26:07 +0300

    Commit 8-index.html 8-styles.css 8-script.js

---
+++ headphones/8-script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("nav-toggle").addEventListener("click", function () {
+    this.classList.toggle("open");
+    document.getElementById("nav-menu").classList.toggle("active");
+});
